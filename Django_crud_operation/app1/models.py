3173226a234536c78f8e94352fa55e41c6f961c7
Nitin Vaishwade <vaishwadenitin@gmail.com>
2017-08-24 17:31:24 +0530

    add first page and form fields

--- Django_crud_operation/app1/models.py
+++ Django_crud_operation/app1/models.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+import datetime
 
-# Create your models here.
+
+class people_detail(models.Model):
+
+    """
+    people details.
+    """
+    # Values for the risk choices
+    
+    username = models.CharField(unique=True, max_length=50)
+    fullname = models.CharField(max_length=100, blank=True)
+    age = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=32, blank=True)
+    state = models.CharField(max_length=32, blank=True)
+    country = models.CharField(max_length=32, blank=True)
+    registered_region = models.CharField(max_length=100, blank=True, null=True)
+    start_date = models.DateTimeField(
+        default=datetime.datetime.now(), null=True, blank=True
+    )
--- Django_crud_operation/app1/views.py
+++ Django_crud_operation/app1/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-	return HttpResponse("<h1>this is app1</h1>")
+def index(request, template="index.html"):
+	return render(request, template,)
